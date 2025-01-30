3d52f712fed30603af63b097585260130f413570
VAIBHAV <101047527+billigoat76@users.noreply.github.com>
2025-01-31 00:07:11 +0530

    dark/light theme

--- lestfound/app/layout.tsx
+++ lestfound/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Navbar } from "@/components/general/Navbar";
+import { ThemeProvider } from "@/components/general/ThemeProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,10 +29,17 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
         <div className="max-w-7xl mx-auto px-4 md:px-6 lg:px-8">
             <Navbar/>
         {children}
         </div>
+        </ThemeProvider>
       </body>
     </html>
   );
--- lestfound/components/general/Navbar.tsx
+++ lestfound/components/general/Navbar.tsx
@@ -1,14 +1,21 @@
 import Link from "next/link";
 import Image from "next/image";
+import { Button } from "../ui/button";
+import { ThemeToggle } from "./ThemeToggle";
 
-export function Navbar(){
-    return(
-        <nav className = "flex justify-between items-center py-5">
-            <Link href = "/" className="flex items-center gap-2">
-                <Image src="/logo.png" alt="logo" width={40} height={40} />
-                <h1 className="text-2xl font-bold">
-                     Lest<span className="text-primary">Found</span></h1>
-            </Link>
-        </nav>
-    )
+export function Navbar() {
+  return (
+    <nav className="flex justify-between items-center py-5">
+      <Link href="/" className="flex items-center gap-2">
+        <Image src="/logo.png" alt="logo" width={40} height={40} />
+        <h1 className="text-2xl font-bold">
+          Lest<span className="text-primary">Found</span>
+        </h1>
+      </Link>
+      <div className="flex items-center gap-4">
+        <ThemeToggle />
+        <Button>Login</Button>
+      </div>
+    </nav>
+  );
 }
