e61b43e213301dfc04cb15536ecb441edadfa90f
VAIBHAV <101047527+billigoat76@users.noreply.github.com>
2025-01-30 23:57:52 +0530

    logo-addition and dark/light mode

--- lestfound/components/general/Navbar.tsx
+++ lestfound/components/general/Navbar.tsx
@@ -1,11 +1,13 @@
 import Link from "next/link";
+import Image from "next/image";
 
 export function Navbar(){
     return(
         <nav className = "flex justify-between items-center py-5">
-            <Link href = "/">
+            <Link href = "/" className="flex items-center gap-2">
+                <Image src="/logo.png" alt="logo" width={40} height={40} />
                 <h1 className="text-2xl font-bold">
-                     Lest <span className="text-primary">Found</span></h1>
+                     Lest<span className="text-primary">Found</span></h1>
             </Link>
         </nav>
     )
